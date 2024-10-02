cca65faee5f0dc7754429600143cfa0f39d875cd
habiba zguaid <zguaidhabiba@gmail.com>
2024-10-02 12:11:26 +0100

    task0

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (err) => {
+      console.error('Redis Client Error', err);
+    });
+
+    // Promisify Redis methods for async/await usage
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return await this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
